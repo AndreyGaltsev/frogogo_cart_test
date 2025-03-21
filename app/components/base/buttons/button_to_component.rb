9f2996f6a109ff2fd8b8a4eb2d9289b56d9cec38
AndreyGaltsev <garry.perm@gmail.com>
2025-03-21 19:21:47 +0500

    fixes after restart

--- app/components/base/buttons/button_to_component.rb
+++ app/components/base/buttons/button_to_component.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class Base::Buttons::ButtonToComponent < ApplicationComponent
-  def initialize(title:, path: nil, method: nil, form_class: nil, render: true, **options)
+  def initialize(title:, path: nil, method: nil, p_class: nil, render: true, **options)
     @title = title
     @path = path
     @method = method
     @render = render
-    @form_class = form_class
+    @p_class = p_class
     @options = options
   end
 
