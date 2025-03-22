a1f983c1030c4b9e79d1e4b9fb84a6b9af62e332
AndreyGaltsev <garry.perm@gmail.com>
2025-03-22 16:16:10 +0500

    fix style

--- app/components/navigation/logo_component.rb
+++ app/components/navigation/logo_component.rb
@@ -3,7 +3,7 @@
 class Navigation::LogoComponent < ApplicationComponent
   erb_template <<-ERB
       <%= link_to root_path, class: 'inline-block my-3' do %>
-        <%= image_tag asset_path('logo-frogogo.png'), alt: 'Frogogo' %>
+        <%= image_tag asset_path('logo-frogogo.png'), alt: 'Frogogo', style: "width: auto; height: 44px;" %>
       <% end %>
   ERB
 
